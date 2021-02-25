b8caab6047728646769438ecb4b13552b5103375
MasterR3C0RD <BrokenR3C0RD@users.noreply.github.com>
2021-02-24 21:57:01 -0400

    Uploaded to demonstrate the failing case

---
+++ examples/failing_case.ts
@@ -0,0 +1,16 @@
+import Parser from "../src/Parser";
+
+let first_parser = new Parser()
+    .string("test1");
+
+let second_parser = new Parser()
+    .string("test2");
+
+let broken_parser = new Parser()
+    .u8("switch")
+    .next("bad", {
+        parser: Parser.choose("switch", {
+            0x0: first_parser,
+            0x1: second_parser
+        })
+    });
